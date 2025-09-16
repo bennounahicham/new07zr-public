e7991f9192547ac8ce956099e20561be8e497fc7
HICHAM\benno <bennounahicham@gmail.com>
2025-09-16 18:24:50 +0100

    slider testemonies

--- js/script.js
+++ js/script.js
@@ -2020,4 +2020,20 @@ $(function () {
     } else {
         $('header').removeClass('scrolled')
     }
+
+            $(".testemonies").slick({
+                infinite: true,
+                speed: 500,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                // autoplay: true,
+                // autoplaySpeed: 3000,
+                arrows: true,
+                centerMode: true,
+                centerPadding: '0px',
+                prevArrow:
+                    '<div class="slick-arrow slick-prev"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle-chevron-left" class="svg-inline--fa fa-circle-chevron-left arrow" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="white" d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0z"/><path fill="#475569" d="M271 135c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-87 87 87 87c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L167 273c-9.4-9.4-9.4-24.6 0-33.9L271 135z"></path></svg></div>',
+                nextArrow:
+                    '<div class="slick-arrow slick-next"><svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle-chevron-right" class="svg-inline--fa fa-circle-chevron-right arrow" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="white" d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256z"/><path fill="#475569" d="M241 377c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l87-87-87-87c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0L345 239c9.4 9.4 9.4 24.6 0 33.9L241 377z"></path></svg></div>',
+            });
 });
